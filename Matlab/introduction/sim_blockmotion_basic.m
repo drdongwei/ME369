81c8724fc0bdaca4772ca31c1066d3b2d3d0adc5
Wei Dong <dr.dongwei@sjtu.edu.cn>
2021-01-15 17:53:47 +0800

    add a basic matlab simulation

---
+++ Matlab/introduction/sim_blockmotion_basic.m
@@ -0,0 +1,43 @@
+
+clear all
+
+figure(1)
+clf
+%%
+%initial time t; postion x; veloctiy v; acceleration a; force F; and mass m;
+t=0; x=0; v=0; a=0; F=0; m=1; 
+
+h=plot(t,x,'Marker','square','MarkerSize',40,'MarkerFaceColor','y','erasemode', 'xor')
+hold on
+ha=plot(t,a,'r','erasemode', 'xor')
+hv=plot(t,v,'g','erasemode', 'xor')
+hx=plot(t,x,'b','erasemode', 'xor')
+xlim([0,6]); ylim([-1,2]);
+legend('mass','a','v','x')
+grid on
+
+%%
+%start simulation
+i=0; dt=0.01; mt=t;ma=a; mv=v; mx=x;
+for i=1:600
+    t=dt*i;
+
+    F=0;
+
+%-----------------------------------------------------------------------------------------
+% controller
+    %F=t;  
+    a=F/m;    v=v+a*dt; x = x+ v*dt +0.5*a*dt^2;
+%-----------------------------------------------------------------------------------------    
+    mt(end+1) =t; ma(end+1)=a;  mv(end+1)=v; mx(end+1)=x;
+    set(h,'xdata',x,'ydata',0)
+    set(ha,'xdata',mt,'ydata',ma)
+    set(hv,'xdata',mt,'ydata',mv)
+    set(hx,'xdata',mt,'ydata',mx)
+    drawnow;                                    
+    pause(dt)
+end
+
+%%
+%end wait to exit
+%pause(5000)
